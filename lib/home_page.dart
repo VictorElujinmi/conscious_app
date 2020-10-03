0818b4a2ce91b79b9b5bae9292bca0bf4f801146
VictorElujinmi <57678101+VictorElujinmi@users.noreply.github.com>
2020-10-03 17:55:04 -0400

    List

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,18 +15,76 @@ class _HomePageState extends State<HomePage> {
           icon: Icon(
             Icons.settings,
             color: Colors.white,),
+
           onPressed: () {
-            //ajhsdfjkasd
+            //Settings
           }
 
-        )
+        ),
+
 
         ]
 
       ),
       body: Center(
-        child: Text('Home Page Flutter Firebase  Content'),
+        child: Location(),
       ),
     );
   }
+}
+class Location extends StatefulWidget{
+  @override
+ _LocationState createState() => _LocationState();
+}
+
+
+class _LocationState extends State<Location> {
+  final _suggestions = <String>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+
+      return Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(10),
+            child: Column (
+              children: <Widget>[
+                TextField(),
+                Location(),
+              ]
+                )
+    )
+      );
+
+
+    }
+
+
+  Widget _LocationList() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemCount: 4 * 2,
+        itemBuilder: /*1*/ (context, i) {
+
+          if (i.isOdd) return Divider(); /*2*/
+          var location = <String>[];
+          final index = i ~/ 2; /*3*/
+
+          _suggestions.addAll(['Bobba Theory', 'Walmart', 'Panama Beach', 'Trader Joes']);
+          /*4*/
+          return _buildRow(_suggestions[index]);
+
+        });
+  }
+
+  Widget _buildRow(String location) {
+    return ListTile(
+      title: Text(
+        location,
+        style: _biggerFont,
+      ),
+    );
+  }
+
 }
